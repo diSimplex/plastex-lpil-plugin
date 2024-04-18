bf6e26138c48e9c054678681f769c54025b640c0
Stephen Gaito <stephen@perceptisys.co.uk>
2024-04-18 10:27:31 +0100

    used PlasTeX plugin initPlugin to implement LPiL magic preamble and postamble comments

--- lpilPlasTeXPlugin/ConfigPlasTeXPlugin.py
+++ lpilPlasTeXPlugin/ConfigPlasTeXPlugin.py
@@ -3,6 +3,7 @@ import os
 import tomllib
 
 from plasTeX.ConfigManager import *
+from plasTeX.Tokenizer import Tokenizer
 
 # from plasTeX.Logging import getLogger
 # pluginLog = getLogger('plugin.loading')
@@ -50,5 +51,46 @@ def updateConfig(config):
         )
       config['lpil']['latexDir'] = latexDir
 
-def initPlugin(config, texStream, texDocument):
-  print("Hello from LPiL Config PlasTeX Plugin : initPlugin")
+def getTokenizerOn(fileName, texStream) :
+  try:
+    encoding = texStream.ownerDocument.config['files'].get('input-encoding', 'utf_8_sig')
+  except (KeyError, AttributeError):
+    encoding = 'utf_8_sig'
+
+  if encoding in ['utf8', 'utf-8', 'utf_8']:
+    encoding = 'utf_8_sig'
+
+  fname = texStream.kpsewhich(fileName)
+  openFile = open(fname, encoding=encoding)
+  return Tokenizer(openFile, texStream.ownerDocument.context)
+
+
+def initPlugin(config, fileName, texStream, texDocument):
+  #print("Hello from LPiL Config PlasTeX Plugin : initPlugin")
+
+  # We implement our LPiL Magic comments
+  # see: https://tex.stackexchange.com/questions/78101/when-and-why-should-i-use-tex-ts-program-and-tex-encoding
+  #
+  # NOTE: All magic comments MUST be BEFORE the first blank line!
+  #       AND must have a '%' as the FIRST character on the line!
+
+  if texStream.toplevel and os.path.isfile(fileName) :
+    with open(fileName) as baseFile :
+      for aLine in baseFile :
+        if not aLine.startswith('%') : break
+        if '!LPiL' not in aLine : continue
+        if '=' in aLine :
+          if 'preamble' in aLine :
+            preAmble = aLine.split('=')[1].strip()
+            if os.path.isfile(preAmble) :
+              #print(f"Found preamble {preAmble}")
+              t = getTokenizerOn(preAmble, texStream)
+              texStream.inputs.append((t, iter(t)))
+              texStream.currentInput = texStream.inputs[-1]
+          elif 'postamble' in aLine :
+            postAmble = aLine.split('=')[1].strip()
+            if os.path.isfile(postAmble) :
+              #print(f"Found postamble {postAmble}")
+              t = getTokenizerOn(postAmble, texStream)
+              texStream.inputs.insert(0, (t, iter(t)))
+              texStream.currentInput = texStream.inputs[-1]
